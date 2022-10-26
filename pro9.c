fb739c85c9a13fad46ad1f49b44c6a1c01a052b3
[azizbenmessaoud] <[bnmessaoudaziz@gmail.com]>
2022-10-26 23:32:19 +0300

    init commit

---
+++ pro9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    int f0 = 0;
+    int f1 = 1;
+    int f = -1;
+
+    scanf("%d\n", &N);
+    printf("f(0) = 0\nf(1) = 1\n");
+
+
+    int k = 2;
+    while (f < N) {
+        k+=1;
+        f = f1 + f0;
+        printf("f(%d) = %d\n", k, f);
+        int a = f1;
+        f1 = f;
+        f0 = f1;
+    }
+
+    return 0;
+}
